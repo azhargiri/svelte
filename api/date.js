75368c486010b09ada85a7aa19f5521c6041636d
Azharuddin <azhargiri@gmail.com>
2021-09-29 13:03:06 +0700

    update

--- api/date.js
+++ api/date.js
@@ -1,4 +1,38 @@
 module.exports = (req, res) => {
-  const date = new Date().toString();
-  res.status(200).send(date);
+  const date = new Date();
+  const timezones = [
+		'UTC',
+		'Asia/Jakarta',
+		'Asia/Makassar',
+		'Asia/Jayapura',
+		'Asia/Kualalumpur',
+		'Asia/Singapore',
+		'Asia/Tokyo',
+		'Asia/Jeddah',
+  ];
+
+	var dates = timezones.map((tz) => {
+		return { 
+			timezone: tz,
+			date: changeTimezone(date, tz).toString() 
+		}	
+	});
+
+  res.status(200).send(JSON.stringify(dates));
 };
+
+// https://stackoverflow.com/questions/15141762/how-to-initialize-a-javascript-date-to-a-particular-time-zone
+function changeTimezone(date, ianatz) {
+
+  // suppose the date is 12:00 UTC
+  var invdate = new Date(date.toLocaleString('en-US', {
+    timeZone: ianatz
+  }));
+
+  // then invdate will be 07:00 in Toronto
+  // and the diff is 5 hours
+  var diff = date.getTime() - invdate.getTime();
+
+  // so 12:00 in Toronto is 17:00 UTC
+  return new Date(date.getTime() - diff); // needs to substract
+}
--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,35 @@
 <script>
   import { onMount } from "svelte";
-  export let date;
+  export let dates = [];
 
   onMount(async () => {
     const res = await fetch("/api/date");
-    const newDate = await res.text();
-    date = newDate;
+    const newDates = await res.json();
+    dates = newDates;
   });
 </script>
 
+<style>
+	h1 {
+		margin-top: -80px;
+		margin-bottom: 10px;
+	}
+	h2 {
+		margin-top: 5px;
+	}
+</style>
+
 <main>
-  <h1>Svelte + Node.js API</h1>
+  <h1>p.e.m.u.d.a</h1>
   <h2>
-    Deployed with
-    <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
-      Vercel
-    </a>
-    !
+	  <img src="/logo.png" alt="Logo Pemuda" />
   </h2>
-  <p>
-    <a
-      href="https://github.com/vercel/vercel/tree/master/examples/svelte"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
   <br />
-  <h2>The date according to Node.js is:</h2>
-  <p>{date ? date : 'Loading date...'}</p>
+  <hr>
+  {#each dates as _date}
+	  <p>{ _date.timezone }: { _date.date }</p>
+  {:else} 
+	<p>Loading...</p>
+  {/each}
+  
 </main>
